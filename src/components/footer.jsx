745aa08fe847ab9944a92553bbdc4680c39b302c
frsargua <frsargua@gmail.com>
2022-08-25 13:18:39 +0100

    created responsive footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+
+import Toolbar from "@mui/material/Toolbar";
+
+import IconButton from "@mui/material/IconButton";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import CallIcon from "@mui/icons-material/Call";
+import EmailIcon from "@mui/icons-material/Email";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { fontSize } from "@mui/system";
+
+export default function Footer() {
+  const theme = createTheme({
+    palette: {
+      whitePrimary: {
+        main: "#fff",
+      },
+    },
+  });
+
+  let arrIcons = [
+    { url: "https://github.com/frsargua", html: <GitHubIcon /> },
+    {
+      url: "https://www.linkedin.com/in/fabiansarango/gi",
+      html: <LinkedInIcon />,
+    },
+    { url: "tel:+00447542351359", html: <EmailIcon /> },
+    { url: "mailto:frsargua@gmail.com", html: <CallIcon /> },
+  ];
+  return (
+    <ThemeProvider theme={theme}>
+      <AppBar
+        className="bg__primary"
+        position="fixed"
+        sx={{
+          bottom: "0",
+          top: "auto",
+          backgroundColor: "black",
+          height: "55px",
+        }}
+      >
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          {arrIcons.map((el) => {
+            return (
+              <IconButton
+                size="s"
+                edge="start"
+                color="whitePrimary"
+                LinkComponent="a"
+                target="_blank"
+                href={el.url}
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                {el.html}
+              </IconButton>
+            );
+          })}
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
