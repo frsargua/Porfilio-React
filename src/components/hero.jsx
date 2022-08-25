d3f90f4dfe051d526d4dee5504a92c2d202cd088
frsargua <frsargua@gmail.com>
2022-08-25 01:25:21 +0100

    added spacing and introduction statement

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -9,11 +9,10 @@ import InteractiveList from "./list";
 export default function PageHero() {
   return (
     <Container maxWidth="xl">
-      <Grid container direction="row">
+      <Grid container direction="row" columnSpacing={4}>
         {/* Increase the network loading priority of the background image. */}
         <Grid
           item
-          columnSpacing={2}
           component="div"
           md={4}
           alt="increase priority"
@@ -33,17 +32,25 @@ export default function PageHero() {
         <Grid
           item
           component="div"
+          sm={12}
           md={8}
-          columnSpacing={2}
           sx={{
             height: "100vh",
-            objectFit: "cover",
             zIndex: "0",
             display: "flex",
-            alignItems: "center",
+            flexDirection: "column",
             justifyContent: "center",
+            alignItems: "center",
           }}
         >
+          <Typography variant="h5" component="p" maxWidth="md">
+            A full-stack web developer majored in Chemistry. Having achieved a
+            full-stack certificate from the University of Birmingham Coding
+            Bootcamp. Adaptable and logic driven problem solver, with strong
+            communication skills and experience working in collaboration or
+            independently. In addition, to being a strong problem solver thanks
+            to my STEM degree, which allowed me to be a fast learner.
+          </Typography>
           <InteractiveList></InteractiveList>
         </Grid>
       </Grid>
