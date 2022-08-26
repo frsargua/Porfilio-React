718ba6a609bbe67f260a9f8a4c96543502a155e7
frsargua <frsargua@gmail.com>
2022-08-26 17:02:38 +0100

    creating redirect functionality

---
+++ src/components/porfolioContainer.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ButtonAppBar from "./navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Porfolio from "./pages/Porfolio";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    return <Porfolio />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <ButtonAppBar
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
