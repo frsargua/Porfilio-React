a207b2b71f64f40ba2c33a76fcaf8d8886ec2232
frsargua <frsargua@gmail.com>
2022-08-27 16:55:27 +0100

    porfolio page

--- src/components/pages/Porfolio.jsx
+++ src/components/pages/Porfolio.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import PorfolioCard from "../card";
+import { largeProjects, smallProjects } from "../../porfolioData";
+import { Container } from "@mui/system";
+import { Grid } from "@mui/material";
 
 export default function Porfolio() {
   return (
-    <div>
+    <>
       <h1>Porfolio</h1>
-    </div>
+      <Container>
+        <Grid
+          container
+          spacing={7}
+          rowGap={4}
+          display="flex"
+          className="block__1"
+          width="100%"
+          sx={{ justifyContent: { sm: "center", md: "left" } }}
+        >
+          {smallProjects.map((el) => {
+            return <PorfolioCard values={el}></PorfolioCard>;
+          })}
+        </Grid>
+      </Container>
+    </>
   );
 }
