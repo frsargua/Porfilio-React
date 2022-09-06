542ee35f3aa0227b18025e2f3395e63648dcfe6c
frsargua <frsargua@gmail.com>
2022-09-06 12:01:51 +0100

    porfolio responsivity updated

--- src/components/pages/Porfolio.jsx
+++ src/components/pages/Porfolio.jsx
@@ -2,31 +2,39 @@ import React from "react";
 import PorfolioCard from "../PorfolioCard";
 import projects from "../../porfolioData";
 import { Container } from "@mui/system";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 
 export default function Porfolio() {
   return (
     <>
       <Container className="block--top__1">
-        <Typography variant="h2" component="h5" textAlign="center">
-          Porfolio
-        </Typography>
-        <Grid
-          container
-          spacing={7}
-          rowGap={2}
-          display="flex"
-          className="block__1"
-          width="100%"
-          sx={{ justifyContent: { sm: "center", md: "left" } }}
-        >
-          {projects.largeProjects.map((el) => {
-            return <PorfolioCard key={el.repoUrl} values={el}></PorfolioCard>;
-          })}
-          {projects.smallProjects.map((el) => {
-            return <PorfolioCard key={el.repoUrl} values={el}></PorfolioCard>;
-          })}
-        </Grid>
+        <Stack alignItems="center">
+          <Typography
+            variant="h2"
+            component="h5"
+            textAlign="center"
+            sx={{ display: { xs: "block", md: "none" } }}
+            gutterBottom
+          >
+            Porfolio
+          </Typography>
+          <Grid
+            container
+            spacing={{ xs: 5 }}
+            rowGap={0}
+            display="flex"
+            className="block__1"
+            width="100%"
+            sx={{ justifyContent: { xs: "center", md: "left" } }}
+          >
+            {projects.largeProjects.map((el) => {
+              return <PorfolioCard key={el.repoUrl} values={el}></PorfolioCard>;
+            })}
+            {projects.smallProjects.map((el) => {
+              return <PorfolioCard key={el.repoUrl} values={el}></PorfolioCard>;
+            })}
+          </Grid>
+        </Stack>
       </Container>
     </>
   );
