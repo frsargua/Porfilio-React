29dc83c242ada2652c59852389e477769d7a0a50
frsargua <frsargua@gmail.com>
2022-09-06 12:01:35 +0100

    Porfolio card styling updated

--- src/components/PorfolioCard.jsx
+++ src/components/PorfolioCard.jsx
@@ -5,50 +5,69 @@ import {
   Button,
   CardMedia,
   Grid,
+  CardContent,
 } from "@mui/material";
+import { Box, Stack } from "@mui/system";
 import React from "react";
 
 export default function PorfolioCard({ values }) {
   return (
-    <Grid item sm={10} md={6} lg={4} component="div" height="400px">
+    <Grid
+      item
+      xs={12}
+      md={6}
+      lg={4}
+      component="div"
+      height={{ xs: "450px", md: "400px" }}
+    >
       <Card
         sx={{
-          display: "flex",
-          flexDirection: "column",
           height: "100%",
           width: "100%",
-          padding: "1rem",
         }}
       >
-        <CardMedia
-          component="img"
-          height="160px"
-          image={require("../images/" + values.imageUrl)}
-          alt="green iguana"
-          sx={{ marginBottom: "1rem", objectFit: "contain" }}
-        />
-        <Typography gutterBottom variant="h5" component="h3" textAlign="center">
-          {values.title}
-        </Typography>
-        <Typography gutterBottom variant="body2" color="text.secondary">
-          {values.description}
-        </Typography>
-        <ButtonGroup
-          // variant="outlined"
-          // color="secondary"
+        <CardContent
           sx={{
             display: "flex",
-            justifyContent: "center",
-            marginTop: "auto",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "90%",
+            width: "100%",
           }}
         >
-          <Button component="a" href={values.repoUrl} target="_blank">
-            Repo
-          </Button>
-          <Button component="a" href={values.deployed} target="_blank">
-            Deployed
-          </Button>
-        </ButtonGroup>
+          <Stack>
+            <CardMedia
+              component="img"
+              height="160px"
+              image={require("../images/" + values.imageUrl)}
+              alt="green iguana"
+              sx={{ marginBottom: "1rem", objectFit: "contain" }}
+            />
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="h3"
+              textAlign="center"
+            >
+              {values.title}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ marginRight: "2rem" }}
+            >
+              {values.description}
+            </Typography>
+          </Stack>
+          <ButtonGroup sx={{ marginX: "auto" }}>
+            <Button component="a" href={values.repoUrl} target="_blank">
+              Repo
+            </Button>
+            <Button component="a" href={values.deployed} target="_blank">
+              Deployed
+            </Button>
+          </ButtonGroup>
+        </CardContent>
       </Card>
     </Grid>
   );
