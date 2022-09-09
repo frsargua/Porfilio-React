0b991912a9bd1235bd9c10c5249e34ed2c386ff5
frsargua <frsargua@gmail.com>
2022-09-09 11:13:29 +0100

    added custom font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
 import "./App.css";
 import PortfolioContainer from "./components/PorfolioContainer";
 
+const theme = createTheme({
+  typography: { fontFamily: ["Poppins"].join(",") },
+});
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <PortfolioContainer></PortfolioContainer>
-    </>
+    </ThemeProvider>
   );
 }
 
